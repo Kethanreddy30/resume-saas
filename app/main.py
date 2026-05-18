f86658a6788621f0334af2d6507cf392a28ae5e7
Kethan Reddy <kethanreddy407@gmail.com>
2026-05-18 12:27:29 +0530

    added Procfile for Render deployment

--- app/main.py
+++ app/main.py
@@ -1,9 +1,8 @@
 from fastapi import FastAPI
-
 from app.routers.health import router as health_router
 from app.routers.profiles import router as profiles_router
 
-app = FastAPI()
+app = FastAPI(title="Resume SaaS API")
 
 app.include_router(health_router)
 app.include_router(profiles_router)
